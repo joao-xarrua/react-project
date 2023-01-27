129524fbbc91b193eeb95c332e9c53677ddf1d61
João Xarrua <joaoxarrua137@gmail.com>
2023-01-27 16:02:55 -0300

    criado userContext referenciando login de usuario

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
+import { UserContext } from "../UserContext";
 
 const Header = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
-    <div className={styles.header}>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Registrar</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login / Registrar
+          </Link>
+        )}
       </nav>
-    </div>
+    </header>
   );
 };
 
