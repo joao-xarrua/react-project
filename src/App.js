91edb665686dddbbe95327d7b61cca1b464bf2fc
João Xarrua <joaoxarrua137@gmail.com>
2023-02-02 09:28:23 -0300

    Prop types e perfil do usuario co

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import Header from "./Components/Header";
 import ProtectedRoute from "./Components/Helper/ProtectedRoute";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
+import NotFound from "./Components/NotFound";
 import Photo from "./Components/Photo/Photo";
 import User from "./Components/User/User";
+import UserProfile from "./Components/User/UserProfile";
 import { UserStorage } from "./UserContext";
 
 const App = () => {
@@ -28,6 +30,8 @@ const App = () => {
               }
             />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
         </UserStorage>
